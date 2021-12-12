937eebcc80de1333ea0a918b4b8dd6d65643e64a
MASTER0811 <85328244+MASTER0811@users.noreply.github.com>
2021-12-12 15:52:28 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function background(writeH,size,position,repeat){
-    let link = document.querySelector(writeH)
-    let link_bg = link.getAttribute("ws-background")
-    link.style.background = link_bg
-    link.style.backgroundSize = size
-    link.style.backgroundPosition = position
-    link.style.backgroundRepeat = repeat
-}
